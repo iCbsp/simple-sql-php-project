193a057c9acc129c2bb1b16f9e0f507f50850351
Carlos <carlos_bsp@hotmail.com>
2020-02-07 09:38:54 +0100

    Map CSS

--- Provincia.php
+++ Provincia.php
@@ -3,42 +3,44 @@
 	<?php include_once("inc/head.php");?>
 
 	<body>
-		<div class="header">
+	<div class="header">
 
-			<?php
-			$mysqli = @new mysqli( 
-				'localhost',   // El servidor 
-				'root',    // El usuario 
-				'',          // La contraseña 
-				'bd_pruebasSQL'); // La base de datos 
+		<?php
+		$mysqli = @new mysqli( 
+			'localhost',   // El servidor 
+			'root',    // El usuario 
+			'',          // La contraseña 
+			'bd_pruebasSQL'); // La base de datos 
 
-			if($mysqli->connect_errno) { 
-				echo '<p>Error al conectar con la base de datos: ' . $mysqli->connect_error; 
-				echo '</p>'; 
-				exit; 
-			}
+		if($mysqli->connect_errno) { 
+			echo '<p>Error al conectar con la base de datos: ' . $mysqli->connect_error; 
+			echo '</p>'; 
+			exit; 
+		}
 
-			 // Ejecuta una sentencia SQL 
-			 $provincias_sentencia = 'SELECT * FROM Provincia WHERE ID = ' . $_GET["ID"]; 
-			 if(!($provincias = $mysqli->query($provincias_sentencia))) { 
-			   echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $mysqli->error . '</p>';
-			   exit; 
-			 } 
-			 echo "<h1><a href='index.php'>";
-			 $coordenadas = NULL;
-			 while($provincia = $provincias->fetch_object()) {
-			 	echo $provincia->Nombre;
-			 	$coordenadas = [$provincia->Longitud, $provincia->Latitud];
-			 }
-			 echo "</a></h1>";
+		 // Ejecuta una sentencia SQL 
+		 $provincias_sentencia = 'SELECT * FROM Provincia WHERE ID = ' . $_GET["ID"]; 
+		 if(!($provincias = $mysqli->query($provincias_sentencia))) { 
+		   echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $mysqli->error . '</p>';
+		   exit; 
+		 } 
+		 echo "<h1><a href='index.php'>";
+		 $coordenadas = NULL;
+		 while($provincia = $provincias->fetch_object()) {
+		 	echo $provincia->Nombre;
+		 	$coordenadas = [$provincia->Longitud, $provincia->Latitud];
+		 }
+		 echo "</a></h1>";
 
-			?>
+		?>
 
-			<p>Texto</p>
+		<p>Texto</p>
 
-		</div>
-
-	<div id="map" class="map"></div>
+	</div>
+	<div class="mapContainer">	
+		<h2>Mapa</h2>
+		<div id="map" class="map"></div>
+	</div>
     <script type="text/javascript">
       var map = new ol.Map({
         target: 'map',
